c1ac57664b75f2b17870f248019499e6f48e95e5
AntoniuCe <141710147+AntoniuCe@users.noreply.github.com>
2024-05-27 18:07:16 +0300

    Update requestforcontinentaljobs.py

--- requestforcontinentaljobs.py
+++ requestforcontinentaljobs.py
@@ -1,41 +1,56 @@
 import requests
 import json
+from openpyxl import Workbook
 
-payload = {
-'tx_conjobs_api[filter][locationSuggestChecksums][]': '9239c9b9130658261ab51795372bd7aa',
-'tx_conjobs_api[filter][searchTerm]':'Internship',
-'tx_conjobs_api[itemsPerPage]':'100'
-}
-# Make the POST request to the API endpoint
-url = "https://jobs.continental.com/ro/api/result-list/pagetype-jobs/"
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-}
+wb = Workbook()
 
-response = requests.get(url,params=payload,headers=headers,verify=False)
-json_data = response.json()
+ws = wb.active
 
-if "result" in json_data:
-    # Extract the list of results
-    job_list = json_data["result"]
+wsConti = wb.create_sheet("Conti ", 0)
+wsNokia = wb.create_sheet("Nokia ", 1)
+wsZF = wb.create_sheet("ZF ", 2)
 
-    # Specify the file path to save the JSON file
-    file_path = "jobs_data.json"
+'''wsConti = wb["Continental jobs"]
+wsNokia = wb["Nokia jobs"]
+wsZF = wb["ZF jobs"]'''
 
-    # Save the JSON data to a file
-    with open(file_path, "w") as json_file:
-        json.dump(job_list, json_file, indent=4)
 
-    print(f"JSON data saved successfully to {file_path}")
-else:
-    print("No list of results found in the JSON data.")
+def conti():
+    payload = {
+        'tx_conjobs_api[filter][locationSuggestChecksums][]': '2525a20f75835ba068814ce776b101b2',
+        'tx_conjobs_api[filter][searchTerm]': 'Internship',
+        'tx_conjobs_api[itemsPerPage]': '100'
+    }
+    url = "https://jobs.continental.com/ro/api/result-list/pagetype-jobs/"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    }
 
-with open('jobs_data.json') as f:
-    data = json.load(f)
-with open('results.txt', 'w') as f:
-    for item in data['list']:
-        f.write(item['title']+'\n')
-        f.write(item['publicationDate']+'\n')
-        f.write('https://jobs.continental.com/en/detail-page/job-detail/'+str(item['url'])+'\n')
-        f.write('\n')
-        f.write('\n')
+    response = requests.get(url, params=payload, headers=headers, verify=False)
+    json_data = response.json()
+
+    if "result" in json_data:
+        job_list = json_data["result"]
+
+        file_path = "jobs_data.json"
+
+        with open(file_path, "w") as json_file:
+            json.dump(job_list, json_file, indent=4)
+
+        print(f"datele json s-au salvat in {file_path}")
+    else:
+        print("nu s-o gasit nicio lista de date in json")
+
+    with open('jobs_data.json') as f:
+        data = json.load(f)
+
+        headers = list(data['list'][0].keys())
+        wsConti.append(headers)
+    for row_data in data['list']:
+        row = [row_data[key] for key in headers]
+        wsConti.append(row)
+
+
+
+conti()
+wb.save("jobs.xlsx")
